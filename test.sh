795ed548248d0831c5f404b817edc8dfacbf065c
rencho13 <stephone13@gmail.com>
2018-12-18 21:49:20 +0100

    Version ok

--- test.sh
+++ test.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-messageAide='USAGE : langstat [FILE] [OPTION]\n OPTION = vide , tri décroissant \n OPTION = -d , tri croissant \n OPTION [A-Z] Stat sur une lettre'
-
-if [ $# = "0" ]; then 
-	echo "fichier manquant en paramètre"
-	echo -e $messageAide
-	exit
-elif [ -z $1 ]; then
-	echo "Paramtre manquant "
-	echo -e $messageAide
-	exit
-elif [ ! -f $1 ]; then
-	echo "Le fichier n'existe pas"
-	echo -e $messageAide
-	exit
-fi
-
 statLettre() 
 {
 echo `touch stat`
@@ -24,15 +8,28 @@ for L in {A..Z}; do
 	echo "`grep -c $L $1` - $L " >> stat
 done
 }
-if [ "S2" != "-d" ]; then
-	echo "`grep -c $2 $1` - $2 "
-	
+
+# Message d'aide qui s'affiche dans le cas d'une erreur
+messageAide='USAGE : langstat [FILE] [OPTION]\n OPTION = vide , tri décroissant \n OPTION = -d , tri croissant'
+
+if [ $# = "0" ]; then # Test si il y a kkchose de saisi en paramètre
+	echo "ERREUR : Il n'y pas de fichier en paramètre"
+	echo -e $messageAide
+	exit
+elif [ $2 != "-d" ]; then # Test si le second paramètre passé en valide 
+	echo "ERREUR : Le second paramètre n'est pas bon "
+	echo -e $messageAide
+	exit
+elif [ ! -f $1 ]; then # Test si le fichier passé en paramètre existe
+	echo "ERREUR : Le fichier n'existe pas "
+	echo -e $messageAide
+	exit
 elif [ "$2" = "-d" ]; then
 	statLettre $1 $2
 	sort -n stat | less
 	rm -f stat
 else
-	statLettre $1 
+	statLettre $1 $2 
     	sort -nr stat | less
 	rm -f stat
 fi

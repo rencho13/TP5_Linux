1faec2d4cff4fec945de2229062c467394a7e306
rencho13 <stephone13@gmail.com>
2018-12-18 11:32:01 +0100

    test existence fichier ok

--- test.sh
+++ test.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-for i in {A..Z};do 
-	echo $i
-done
+if [ -z $1 ]; then
+	echo "Paramtre manquant ( tapez --help )"
+	exit
+fi
+
+if [ ! -f $1 ]; then
+	echo "Le fichier n'existe pas"
+	exit
+fi
+
+if [ $1 = "--help" ];then
+	echo "USAGE: langstat [FILE]..[OPTION]"
+	echo " OPTION =  "
+	exit 
+fi
+
+echo "le nom du fichier est $1 "
 

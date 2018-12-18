91c4d54cd309f5909941ddbc458587ea182e40e8
rencho13 <stephone13@gmail.com>
2018-12-18 15:23:03 +0100

    implementation du parametre -d

--- test.sh
+++ test.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
-	echo "Paramtre manquant ( tapez --help )"
-	exit
-fi
+messageAide='USAGE : langstat [FILE] [OPTION]\n OPTION = vide , tri décroissant \n OPTION = -d , tri croissant \n OPTION [A-Z] Stat sur une lettre'
 
-if [ ! -f $1 ]; then
+if [ $# = "0" ]; then 
+	echo "fichier manquant en paramètre"
+	echo -e $messageAide
+	exit
+elif [ -z $1 ]; then
+	echo "Paramtre manquant "
+	echo -e $messageAide
+	exit
+elif [ ! -f $1 ]; then
 	echo "Le fichier n'existe pas"
+	echo -e $messageAide
 	exit
 fi
 
-if [ $1 = "--help" ];then
-	echo "USAGE: langstat [FILE]..[OPTION]"
-	echo " OPTION =  "
-	exit 
+statLettre() 
+{
+echo `touch stat`
+
+for L in {A..Z}; do
+	echo "`grep -c $L $1` - $L " >> stat
+done
+}
+
+
+if [ "$2" = "-d" ]; then
+	statLettre $1 $2
+	sort -n stat | less
+	rm -f stat
+else
+	statLettre $1 $2
+    	sort -nr stat | less
+	rm -f stat
 fi
-
-echo "le nom du fichier est $1 "
-
